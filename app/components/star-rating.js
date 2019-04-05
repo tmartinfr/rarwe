3beec4d0edadd1bf542ade6bdf9ee573911272f5
Thomas Martin <thomas@oopss.org>
2019-04-05 16:01:38 +0200

    Add stars

---
+++ app/components/star-rating.js
@@ -0,0 +1,15 @@
+import { computed } from '@ember/object';
+import Component from '@ember/component';
+
+export default Component.extend({
+    classNames: ['rating-panel'],
+    rating: 0,
+    maxRating: 5,
+    stars: computed('rating', 'maxRating', function() {
+        let stars = [];
+        for(let i=1; i <= this.maxRating; i++) {
+            stars.push({rating: i, isFull: this.rating >= i });
+        }
+        return stars;
+    }),
+});
