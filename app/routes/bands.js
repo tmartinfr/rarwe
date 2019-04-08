251a573500dd8ad53aff511bbe152c85b94d0675
Thomas Martin <thomas@oopss.org>
2019-04-08 16:23:03 +0200

    Add band detail

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -25,7 +25,7 @@ export default Route.extend({
             rating: 5
         });
         let ledZeppelin = Band.create({ name: 'Led Zeppelin', songs: [blackDog] });
-        let pearlJam = Band.create({ name: 'Pearl Jam', songs: [yellowLedbetter, daughter] });
+        let pearlJam = Band.create({ name: 'Pearl Jam', description: 'Pearl Jam is an American rock band, formed in Seattle, Washington in 1990.', songs: [yellowLedbetter, daughter] });
         let fooFighters = Band.create({ name: 'Foo Fighters', songs: [pretender] });
         return [ledZeppelin, pearlJam, fooFighters];
     }
--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
+// import { isEmpty } from '@ember/utils';
 
 export default Route.extend({
     model(params) {
         let bands = this.modelFor('bands');
         return bands.findBy('slug', params.slug);
-    }
+    },
 });
