3a79b4a79a04155c53a2ff756b36838a372ce347
Thomas Martin <thomas@oopss.org>
2019-04-08 16:33:39 +0200

    Reset song widget

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -4,4 +4,10 @@ export default Route.extend({
     // model() {
     //     return this.modelFor('bands.band');
     // }
+    resetController(controller) {
+        controller.setProperties({
+            'isAddingSong': false,
+            'newSongName': '',
+        });
+    }
 });
