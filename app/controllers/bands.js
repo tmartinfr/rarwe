03b50fded13bc48f8c64575fb9a54479e21437ef
Thomas Martin <thomas@oopss.org>
2019-04-08 16:27:59 +0200

    Bypass model resolution skipping

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -22,7 +22,7 @@ export default Controller.extend({
                 newBandName: '',
                 isAddingBand: false
             });
-            this.transitionToRoute('bands.band.songs', newBand);
+            this.transitionToRoute('bands.band.songs', newBand.slug);
         }
     }
 });
