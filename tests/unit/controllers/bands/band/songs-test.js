43643407541468b63f92b2130dbf5e569eeb2e77
Thomas Martin <thomas@oopss.org>
2019-04-18 11:00:41 +0200

    Add model test

--- tests/unit/controllers/bands/band/songs-test.js
+++ tests/unit/controllers/bands/band/songs-test.js
@@ -8,7 +8,7 @@ module('Unit | Controller | bands/band/songs', function(hooks) {
   test('isAddButtonDisabled', function(assert) {
     let controller = this.owner.lookup('controller:bands/band/songs');
     controller.set('newSongName', 'Belenos');
-    assert.notOk(controller.get('isAddButtonDisabled'), 'The button is not disabled when there is a title'));
+    assert.notOk(controller.get('isAddButtonDisabled'), 'The button is not disabled when there is a title');
     controller.set('newSongName', '');
     assert.ok(controller.get('isAddButtonDisabled'), 'The button is disabled when the title is empty');
   });
